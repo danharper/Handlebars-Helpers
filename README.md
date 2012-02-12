342558811620895e8e7711945abfaa36349a380a
Dan Harper <intouch@danharper.me>
2012-02-12 20:28:50 +0000

    Let's get helping

---
+++ README.md
@@ -0,0 +1,43 @@
+# Handlebars Helpers
+
+A small collection of useful helpers for [Handlebars.js](https://github.com/wycats/handlebars.js).
+
+## Equals
+
+### If x Equals y
+`{{#if_eq x compare=y}} ... {{/if_eq}}`
+
+### Unless x Equals y
+`{{#unless_eq x compare=y}} ... {{/unless_eq}}`
+
+## Greater Than
+
+### If x > y
+`{{#if_gt x compare=y}} ... {{/if_gt}}`
+
+### Unless x > y
+`{{#unless_gt x compare=y}} ... {{/unless_gt}}`
+
+## Greater Than or Equal To
+
+### If x >= y
+`{{#if_gteq x compare=y}} ... {{/if_gteq}}`
+
+### Unless x >= y
+`{{#unless_gteq x compare=y}} ... {{/unless_gteq}}`
+
+## Less Than
+
+### If x < y
+`{{#if_lt x compare=y}} ... {{/if_lt}}`
+
+### Unless x < y
+`{{#unless_lt x compare=y}} ... {{/unless_lt}}`
+
+## Less Than or Equal To
+
+### If x <= y
+`{{#if_lteq x compare=y}} ... {{/if_lteq}}`
+
+### Unless x <= y
+`{{#unless_lteq x compare=y}} ... {{/unless_lteq}}`
---
+++ helpers.js
@@ -0,0 +1,103 @@
+/**
+ * If Equals
+ * if_eq this compare=that
+ */
+Handlebars.registerHelper('if_eq', function(context, options) {
+	if (context == options.hash.compare)
+		return options.fn(context);
+	return options.inverse(context);
+});
+
+/**
+ * Unless Equals
+ * unless_eq this compare=that
+ */
+Handlebars.registerHelper('unless_eq', function(context, options) {
+	if (context == options.hash.compare)
+		return options.unless(context);
+	return options.fn(context);
+});
+
+
+/**
+ * If Greater Than
+ * if_gt this compare=that
+ */
+Handlebars.registerHelper('if_gt', function(context, options) {
+	if (context > options.hash.compare)
+		return options.fn(context);
+	return options.inverse(context);
+});
+
+/**
+ * Unless Greater Than
+ * unless_gt this compare=that
+ */
+Handlebars.registerHelper('unless_gt', function(context, options) {
+	if (context > options.hash.compare)
+		return options.unless(context);
+	return options.fn(context);
+});
+
+
+/**
+ * If Less Than
+ * if_lt this compare=that
+ */
+Handlebars.registerHelper('if_lt', function(context, options) {
+	if (context < options.hash.compare)
+		return options.fn(context);
+	return options.inverse(context);
+});
+
+/**
+ * Unless Less Than
+ * unless_lt this compare=that
+ */
+Handlebars.registerHelper('unless_lt', function(context, options) {
+	if (context < options.hash.compare)
+		return options.unless(context);
+	return options.fn(context);
+});
+
+
+/**
+ * If Greater Than or Equal To
+ * if_gteq this compare=that
+ */
+Handlebars.registerHelper('if_gteq', function(context, options) {
+	if (context >= options.hash.compare)
+		return options.fn(context);
+	return options.inverse(context);
+});
+
+/**
+ * Unless Greater Than or Equal To
+ * unless_gteq this compare=that
+ */
+Handlebars.registerHelper('unless_gteq', function(context, options) {
+	if (context >= options.hash.compare)
+		return options.unless(context);
+	return options.fn(context);
+});
+
+
+/**
+ * If Less Than or Equal To
+ * if_lteq this compare=that
+ */
+Handlebars.registerHelper('if_lteq', function(context, options) {
+	if (context <= options.hash.compare)
+		return options.fn(context);
+	return options.inverse(context);
+});
+
+/**
+ * Unless Less Than or Equal To
+ * unless_lteq this compare=that
+ */
+Handlebars.registerHelper('unless_lteq', function(context, options) {
+	if (context <= options.hash.compare)
+		return options.unless(context);
+	return options.fn(context);
+});
